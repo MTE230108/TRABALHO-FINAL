20e8ef31bc075e4646508ea6b3dbc9f6e787911f
MTE <mateus_espindola1@estudante.sesisenai.org.br>
2024-09-30 20:50:35 -0300

    banco de dados nuevo

--- backend/arquivos banco trabalho final.sql
+++ backend/arquivos banco trabalho final.sql
@@ -26,28 +26,31 @@ CREATE TABLE IF NOT EXISTS `clientes` (
   `idade` int(11) DEFAULT NULL,
   `email` varchar(50) DEFAULT NULL,
   `telefone` varchar(50) DEFAULT NULL,
-  `jogo` varchar(50) DEFAULT NULL,
   `cpf` varchar(50) DEFAULT NULL,
   `jogo` varchar(50) DEFAULT NULL,
-  `hora_de_jogo` int(11) DEFAULT NULL,
+  `hora_de_devolucao` int(11) DEFAULT NULL,
   PRIMARY KEY (`id_cliente`) USING BTREE
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=26 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- Copiando dados para a tabela backend.clientes: ~0 rows (aproximadamente)
+-- Copiando dados para a tabela backend.clientes: ~1 rows (aproximadamente)
 DELETE FROM `clientes`;
+INSERT INTO `clientes` (`id_cliente`, `nome`, `idade`, `email`, `telefone`, `cpf`, `jogo`, `hora_de_devolucao`) VALUES
+	(25, 'Mateus', 16, '@mdaiogo', '3123425', '1245312', 'Pokemon', 12);
 
--- Copiando estrutura para tabela backend.comida
-CREATE TABLE IF NOT EXISTS `comida` (
+-- Copiando estrutura para tabela backend.comidas
+CREATE TABLE IF NOT EXISTS `comidas` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `nome` varchar(50) DEFAULT NULL,
   `preco` decimal(6,2) DEFAULT NULL,
-  `validade` datetime DEFAULT NULL,
+  `validade` decimal(20,6) DEFAULT NULL,
   `quantidade` int(11) DEFAULT NULL,
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=19 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- Copiando dados para a tabela backend.comida: ~0 rows (aproximadamente)
-DELETE FROM `comida`;
+-- Copiando dados para a tabela backend.comidas: ~1 rows (aproximadamente)
+DELETE FROM `comidas`;
+INSERT INTO `comidas` (`id`, `nome`, `preco`, `validade`, `quantidade`) VALUES
+	(18, 'Jaca', 8.00, 12.000000, 23);
 
 -- Copiando estrutura para tabela backend.jogos
 CREATE TABLE IF NOT EXISTS `jogos` (
@@ -56,14 +59,16 @@ CREATE TABLE IF NOT EXISTS `jogos` (
   `preco` decimal(20,6) NOT NULL DEFAULT 0.000000,
   `ano_publicacao` int(11) NOT NULL DEFAULT 0,
   `editora` varchar(50) NOT NULL DEFAULT '0',
-  `alugados` tinyint(4) NOT NULL DEFAULT 0,
+  `alugados` varchar(50) NOT NULL DEFAULT '0',
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- Copiando dados para a tabela backend.jogos: ~1 rows (aproximadamente)
+-- Copiando dados para a tabela backend.jogos: ~3 rows (aproximadamente)
 DELETE FROM `jogos`;
 INSERT INTO `jogos` (`id`, `titulo`, `preco`, `ano_publicacao`, `editora`, `alugados`) VALUES
-	(1, 'timertops', 12.000000, 1234, 'rego', 20);
+	(5, 'Mordaw', 133.000000, 12, 'Maco', 'sim'),
+	(6, 'Rim World', 234.000000, 23, 'serian', 'não'),
+	(7, 'Paramont', 145.000000, 42, 'Bro', 'não');
 
 /*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
